5598d845a0eb0dbd9b6832c3ea04243f47cc287e
lilmisfortune <160452073+lilmisfortune@users.noreply.github.com>
2024-02-19 12:05:14 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+from flask import FLask,request,rendeer_template
+app = Flask(_name_）
+@app.route("/",method = ["GET","POST"]
+def index():
+  return(render_template("index.html")
+if __name__ == "__main__":
+   app.run()
